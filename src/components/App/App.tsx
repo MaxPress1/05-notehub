634a477e9fe72a362de933bbbfbfff935c075ce2
MaxPress <maxpress96@ukr.net>
2025-06-12 07:08:13 +0300

    pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import css from "./App.module.css";
 import { fetchNotes } from "../../services/noteService";
-import NoteList from "../NodeList/NoteList";
+import NoteList from "../NoteList/NoteList";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import ReactPaginate from "react-paginate";
 import NoteModal from "../NoteModal/NoteModal";
-import SearchBox from "../../SearchBox/SearchBox";
+import SearchBox from "../SearchBox/SearchBox";
 import { useDebounce } from "use-debounce";
+import Pagination from "../Pagination/Pagination";
 
 export default function App() {
   const [page, setPage] = useState(1);
@@ -27,27 +27,19 @@ export default function App() {
     <div className={css.app}>
       <header className={css.toolbar}>
         {data && !isLoading && <NoteList notes={data.notes} />}
-        {isSuccess && data.totalPages > 1 && (
-          <ReactPaginate
-            pageCount={data.totalPages}
-            pageRangeDisplayed={5}
-            marginPagesDisplayed={1}
-            onPageChange={({ selected }) => setPage(selected + 1)}
-            forcePage={page - 1}
-            containerClassName={css.pagination}
-            activeClassName={css.active}
-            nextLabel="→"
-            previousLabel="←"
-          />
-        )}
-
         <SearchBox
           value={SearchText}
           onChange={(value: string) => {
             setSearchText(value);
           }}
         />
-
+        {isSuccess && data.totalPages > 1 && (
+          <Pagination
+            page={page}
+            totalPages={data.totalPages}
+            onPageChange={setPage}
+          />
+        )}
         <button className={css.button} onClick={() => setIsModalOpen(true)}>
           Create note +
         </button>
